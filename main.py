2ef863c394d4f32e55de54ec7ff78636071afbe1
EiriniSkapeti <eiriniskap@gmail.com>
2025-07-23 20:57:23 +0300

    Used basehandler

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 #HTTP server
-print("This is the HTTP server")
+#print("This is the HTTP server")
 
 import http.server
 import socketserver
 
 PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever() 
-
-
+class MyBaseHttpHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/index.html":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+        else:
+            self.send_response(404)
+            self.end_headers()
+          
+with socketserver.TCPServer(("", PORT), MyBaseHttpHandler) as httpd:
+    print(f"Serving at port {PORT}")
+    httpd.serve_forever()
